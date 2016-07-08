81804cc3a649337a6a5cc8614cfbc5da04e4a0df
Caroline Weinberg <weinberg.caroline@gmail.com>
2016-07-08 15:49:52 -0400

    changed setup file. Now compiles after running twice

--- setup.sh
+++ setup.sh
@@ -1,23 +1,59 @@
 #!/bin/bash
+
+abort()
+{
+	echo >&2 '
+		ABORTED!!!! Setup did not complete successfully
+		'
+	exit 1
+}
+trap 'abort' 0
+
+set -e
 MY_PATH=$PWD
 
 wstool update
 
 source /opt/ros/indigo/setup.bash
 
+cd ${MY_PATH}/graspit_bci_ws/src
+if [ -f ${MY_PATH}/graspit_bci_ws/src/CMakeLists.txt ]; then
+	source ${MY_PATH}/graspit_bci_ws/devel/setup.bash	
+else
+	catkin_init_workspace	
+fi
+
+cd ${MY_PATH}/graspit_bci_ws/
+catkin_make
+
+
+
 cd ${MY_PATH}/jaco_ws/src
-catkin_init_workspace
+if [ -f ${MY_PATH}/jaco_ws/src/CMakeLists.txt ]; then
+	source ${MY_PATH}/jaco_ws/devel/setup.bash
+else
+	catkin_init_workspace
+fi
+
 cd ${MY_PATH}/jaco_ws
 catkin_make
 
+source ${MY_PATH}/jaco_ws/devel/setup.bash
+
+
+
 cd ${MY_PATH}/bci_project_ws/src
-catkin_init_workspace
+if [ -f ${MY_PATH}/bci_project_ws/src/CMakeLists.txt ]; then
+	source ${MY_PATH}/bci_project_ws/devel/setup.bash
+else
+	catkin_init_workspace
+fi
+
 cd ${MY_PATH}/bci_project_ws
 catkin_make
 
-source devel/setup.bash
+trap : 0
 
-cd ${MY_PATH}/graspit_bci_ws/src
-catkin_init_workspace
-cd ${MY_PATH}/graspit_bci_ws/
-catkin_make
+
+
+
