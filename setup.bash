dae2f6f6dcfd798b7c7df1b94018072e9666b31a
David Watkins <djrival7@gmail.com>
2017-03-23 05:10:01 -0400

    Removed erroneous comment from setup.bash

--- setup.bash
+++ setup.bash
@@ -2,6 +2,6 @@ export ROS_HOSTNAME=$(hostname).local
 export ROS_MASTER_URI=http://bali.local:11311
 
 source bci_project_ws/devel/setup.bash
-#source graspit_bci_ws/devel/setup.bash
+source graspit_bci_ws/devel/setup.bash
 source jaco_ws/devel/setup.bash --extend
 source ../kinect2/devel/setup.bash --extend
